3c7efe2402f6ecafdf9e3390bb9c0d756c1b560d
john2306 <johnmendozauni@gmail.com>
2023-02-20 10:27:01 -0500

    Add Test

---
+++ test.py
@@ -0,0 +1,26 @@
+# python manage.py test EasyExcelAPIView
+from django.test import TestCase
+
+
+class EasyExcelAPIViewTestCase(TestCase):
+    """
+    Testing para el módulo EasyExcelAPIView
+    """
+    def setUp(self):
+        """
+        Preparar el testing
+        """
+        pass
+
+    def test_easy_excel_api_view(self):
+        """
+        Testing para el módulo EasyExcelAPIView
+        """
+        pass
+
+    def tearDown(self):
+        """
+        Finalizar el testing
+        """
+        pass
+
